f02c617c0b72c86aa1de0c8c136315dbecfda09b
ac_orz <hank710535@gmail.com>
2026-03-01 00:22:02 +0800

    logo pptx update

--- APP/obj/Release/net10.0-windows/APP.AssemblyInfo.cs
+++ APP/obj/Release/net10.0-windows/APP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ed3c2512f34c395b09af031e4c9987bdadc62e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eb6df24f730954c1de772e649d4ce6d51d6492d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
